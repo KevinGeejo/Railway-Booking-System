95bfacdeb60681023a567fb00d45b985c7f2612f
birdpeople <bird_people@126.com>
2021-05-21 22:01:50 +0800

    update_data
    update_data

--- modify_csv/m_csv.m
+++ modify_csv/m_csv.m
@@ -1,7 +1,7 @@
 clear;
 add = '/Users/birdpeople/classObject/DB/train/';
 file = {'0';'g';'t';'c';'k';'y';'z';'d'}';
-
+%file = {'0';'g'}';
 for this = 1:size(file,2)
 address = strcat(add,file{this},'/');
 File = dir(fullfile(address,'*.csv'));
@@ -10,17 +10,33 @@ Length_Names = size(FileNames,1);
 
 global B;
 for i = 1:Length_Names
+    B = string([]);
+    C = string([]);
      K_Trace = strcat(address, FileNames(i));
      A = readmatrix(K_Trace{1}, 'OutputType', 'string');
-     B = A(:,[1,2,3,4,8,9,10]);
+     BB = A(:,[1,2,3,4,6,8,9,10,]);
+     
+      CC = getSeat(BB);
+     num = size(BB,1);
+     index = 1;
+     for ii = 1:num
+         if ~(BB(ii,5) ~= '-' && str2double(BB(ii,5))< 0 && CC(ii,1)=='0'&&CC(ii,2)=='0'&&CC(ii,3)=='0'&&CC(ii,4)=='0'&&CC(ii,5)=='0'&&CC(ii,6)=='0'&&CC(ii,7)=='0')
+             B(index,:) = BB(ii,[1,2,3,4,6,7,8]); 
+             C(index,:) = CC(ii,:);index = index+1;
+         end
+     end
+     
      mnptime(B);
-     C = getSeat(B);
+     
+    
      D = [B(2:end,[1,2,3,4]),C(2:end,:)];
      g = strsplit(FileNames{i},'.');
      E = string(zeros(size(B,1)-1,1));
      for col = 1:(size(B,1)-1)
          E(col,1) = g{1}; 
      end
+     
+     
     if i==1 && this ==1
         Final = [E,D];
         F = [];
@@ -32,17 +48,28 @@ for i = 1:Length_Names
         Fi = [E(1,1),B(1,2),B(end,2),B(1,4)];
         FinalInfo = [FinalInfo;Fi];
     end
-    clear A,B,C,D,E,F,K_Trace,g,Fi;
+    
+   
+    
+    clear A,B,C,D,E,F,K_Trace,g,Fi,BB,CC;
     
 end
 
 end
 
+ Fu = string([]);inde = 1;
+   
+    for m = 1: size(Final,1)
+        if ~(Final(m,6) == '0' && Final(m,7) == '0' && Final(m,8) == '0' && Final(m,9) == '0' && Final(m,10) == '0' && Final(m,11) == '0' && Final(m,12) == '0')
+            Fu(inde,:) = Final(m,:); inde = inde + 1;
+        end
+    end
+    
 newname = '_trainItem.csv';
 newnameinfo = '_trainstaticInfo.csv';
 
 cell2csv(strcat(add,newnameinfo),FinalInfo,',');
-cell2csv(strcat(add,newname),Final,',');
+cell2csv(strcat(add,newname),Fu,',');
 
 function  mnptime(B)
 global B;
@@ -55,9 +82,9 @@ end
 
 function C = getSeat(B)
     for i = 1:size(B,1)
-        tmp1 = strsplit(B(i,5),'/');
-        tmp2 = strsplit(B(i,6),'/');
-        tmp3 = strsplit(B(i,7),'/');
+        tmp1 = strsplit(B(i,6),'/');
+        tmp2 = strsplit(B(i,7),'/');
+        tmp3 = strsplit(B(i,8),'/');
         C(i,[1,2]) =tmp1;
         C(i,[3,4,5]) =tmp2;
         C(i,[6,7]) =tmp3;
