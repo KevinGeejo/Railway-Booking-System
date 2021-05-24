605bbb98a76f95f7e298af738aad646e975e032f
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2021-05-24 15:49:32 +0800

    delete app query test

--- web/query_city_and_station/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QueryCityAndStationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'query_city_and_station'
--- web/query_city_and_station/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("在这里, 你可以查询火车站与城市的对应关系.")
